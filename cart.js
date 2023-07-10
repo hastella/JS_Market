fd4954dff205f0b3c4f0d6ea2c58e0f33215d15c
Stella_Ha <sangyeeh@gmail.com>
2023-07-11 02:02:50 +0900

    :memo: removeCartCallback 함수 정리노트 추가
     removeCartCallback 함수 정리노트 추가

--- cart.js
+++ cart.js
@@ -1,11 +1,16 @@
 import { getCartInfo } from "./module/cartToggleBtn.js";
 import { getProductList } from "./module/productList.js";
 import { makeDomWithProperties } from "./utils/dom.js";
+
+// 1. 장바구니에 있는 물품 정보 가져오기
+// 2. 물품 정보 - productList와 연결하기
+// 3. section 아래의 cart-pay-container 앞에 productList를 삽입하기
+
 const sectionDOM = document.getElementsByTagName("section")[0];
 const cartPayContainerDOM = document.getElementById("cart-pay-container");
-const cartInfo = getCartInfo();
 const reloadPage = () => location.reload();
 
+const cartInfo = getCartInfo(); // 1. 장바구니에 있는 물품 정보 가져오기
 if (cartInfo.length < 1) {
   // 장바구니가 비었을 때
   const noticeDOM = makeDomWithProperties("div", {
@@ -14,8 +19,8 @@ if (cartInfo.length < 1) {
   });
   sectionDOM.insertBefore(noticeDOM, cartPayContainerDOM);
 } else {
-  const productListDOM = getProductList(cartInfo, reloadPage);
-  sectionDOM.insertBefore(productListDOM, cartPayContainerDOM);
+  const productListDOM = getProductList(cartInfo, reloadPage); // 2. 물품 정보 - productList와 연결하기
+  sectionDOM.insertBefore(productListDOM, cartPayContainerDOM); // 3. section 아래의 cart-pay-container 앞에 productList를 삽입하기
   // A.insertBefore(B, C);
   // A의 자식 노드 중 C를 찾아서 그 앞에 B를 삽입한다.
 }
