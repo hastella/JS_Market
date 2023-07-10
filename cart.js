93b021005557ebd3a0c39389f47f234c0730e311
Stella_Ha <sangyeeh@gmail.com>
2023-07-11 01:23:51 +0900

    :sparkles: 장바구니 비었을 경우 처리
    장바구니 비었을 경우 처리

--- cart.js
+++ cart.js
@@ -1,17 +1,17 @@
 import { getCartInfo } from "./module/cartToggleBtn.js";
 import { getProductList } from "./module/productList.js";
-
+import { makeDomWithProperties } from "./utils/dom.js";
 const sectionDOM = document.getElementsByTagName("section")[0];
 const cartPayContainerDOM = document.getElementById("cart-pay-container");
 const cartInfo = getCartInfo();
 
 if (cartInfo.length < 1) {
   // 장바구니가 비었을 때
-  const DOM = makeDOMWithProperties("div", {
+  const noticeDOM = makeDomWithProperties("div", {
     innerHTML: "장바구니가 비었습니다.",
     className: "product-list-con",
   });
-  alert("장바구니가 비었습니다.");
+  sectionDOM.insertBefore(noticeDOM, cartPayContainerDOM);
 } else {
   const productListDOM = getProductList(cartInfo);
   sectionDOM.insertBefore(productListDOM, cartPayContainerDOM);
