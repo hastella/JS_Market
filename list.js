ff8a9518350454d0bc63ca98f81165058706df55
Stella_Ha <sangyeeh@gmail.com>
2023-07-12 01:38:59 +0900

    :art: productList fetch함수로 빼주고, reduce 함수로 업데이터
     productList fetch함수로 빼주고, reduce 함수로 업데이트

--- list.js
+++ list.js
@@ -1,20 +1,16 @@
-import { getProductCard } from "./module/productCard.js";
+import { fetchSectionListData } from "./module/fetch.js";
+import { getProductList } from "./module/productList.js";
 
-const sectionDOM = document.getElementsByTagName("section")[0];
+// 물품 목록을 모두 불러와서 페이지에 띄우기 => productList.js getProductList 함수사용!
 
-const productCard = getProductCard({
-  id: 4,
-  imgSrc: "/public/assets/삼겹살.jpg",
-  name: "구이용 삼겹살 600g (냉장)",
-  discountPercent: 5,
-  originalPrice: 15600,
-});
-const productCard2 = getProductCard({
-  id: 5,
-  imgSrc: "/public/assets/머핀.jpg",
-  name: "[홍대 W마켓] 머핀 (2입)",
-  discountPercent: 20,
-  originalPrice: 6000,
-});
-sectionDOM.appendChild(productCard);
-sectionDOM.appendChild(productCard2);
+const sectionInfoList = await fetchSectionListData();
+const productList = sectionInfoList.reduce(
+  //newArr에 curr의 productList를 넣어준다. => prev + curr.productList
+  (prev, curr) => [...prev, ...curr.productList],
+  []
+);
+
+const section = document.getElementsByTagName("section")[0];
+const productListDOM = getProductList(productList);
+section.appendChild(productListDOM);
+// productListDOM을 section에 넣어준다.
--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-import { getProductList } from "./module/productList.js";
+import { fetchSectionListData } from "./module/fetch.js";
 import { getProductSection } from "./module/productSection.js";
 
 const body = document.getElementsByTagName("body")[0];
 try {
-  const response = await fetch("public/mock/sectionListData.json");
-  const data = await response.json();
-  const sectionInfoList = data.sectionInfoList;
-
+  const sectionInfoList = await fetchSectionListData();
   sectionInfoList.forEach((sectionInfo) => {
     console.log(sectionInfo);
     const { sectionTitle, productList } = sectionInfo;
